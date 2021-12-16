437173f292ef4624187e88d471051237548d45a0
Tano Martin <tanomartin3@gmail.com>
2021-12-16 11:03:48 +0000

    add file

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+def scrap_anim(current_page=None):
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    scrap_anim()
